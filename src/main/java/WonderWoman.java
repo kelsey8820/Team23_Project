167c8cf9930aa2987bb733ff3bb3d92a420f1c25
cmert2017 <cihanmert2075@gmail.com>
2020-11-01 14:44:42 -0500

    Second commit

---
+++ src/main/java/WonderWoman.java
@@ -0,0 +1,5 @@
+public class WonderWoman {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/SuperGirl.java
@@ -0,0 +1,5 @@
+public class SuperGirl {
+    public static void main(String[] args) {
+
+    }
+}
